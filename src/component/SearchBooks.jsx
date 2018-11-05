9eccc405f8881a7f029f51e58be379267c6f7101
Amjad Khan <amjadk12@gmail.com>
2018-11-05 22:04:44 +0530

    Refactored the SearchBooks component, set few more test cases with Pass, added ignore --coverage for serviceWorker.js and index.js -- check for same text search  twice

--- src/component/SearchBooks.jsx
+++ src/component/SearchBooks.jsx
@@ -1,9 +1,49 @@
 import React, { Component } from "react";
 
 export default class SearchBooks extends Component {
-  state = {};
+  state = {
+    searchText: "",
+    prevSearchText: "",
+    error: ""
+  };
 
+  onTextChange = e => {
+    this.setState({ searchText: e.target.value.trim() });
+  };
+  // Avoid un-neccesary calls - Already searched or Blank search
+  handleClick = async () => {
+    if (
+      (this.state.prevSearchText.toLocaleLowerCase() !==
+        this.state.searchText.toLocaleLowerCase() ||
+        this.state.prevSearchText === "") &&
+      this.state.searchText !== ""
+    ) {
+      this.setState({ prevSearchText: this.state.searchText });
+      //Need to call the search api
+    } else {
+      alert("already searched or blank search");
+    }
+  };
   render() {
-    return <div>Search controls here</div>;
+    return (
+      <div>
+        <h1>Search</h1>
+        <input
+          className="searchtext mr-1 col-sm-9 form-control"
+          type="text"
+          placeholder="Search Books By title or author or ISBN"
+          name="searchText"
+          onChange={this.onTextChange}
+          value={this.state.searchText}
+        />
+        <button
+          className="search col-sm-2 btn btn-primary"
+          id="search"
+          onClick={this.handleClick}
+        >
+          Search
+        </button>
+      </div>
+    );
   }
 }
--- src/tests/component/SearchBooks.test.js
+++ src/tests/component/SearchBooks.test.js
@@ -18,7 +18,45 @@ describe("SearchBooks", () => {
   it("should render a <button />", () => {
     expect(wrapper.find("button").length).toEqual(1);
   });
-  it("should have text on <button /> as Search", () => {
+  it("should render a <button />", () => {
     expect(wrapper.find("button.search").text()).toEqual("Search");
+  });
+  it("should call onChange prop with input value", () => {
+    const onSearchMock = jest.fn();
+    const wrapper = mount(
+      <input className="mockInput" onChange={onSearchMock} value="jungle" />
+    );
+    wrapper.find("input").simulate("change", "test");
+    expect(wrapper.find("input.mockInput").props().value).toEqual("jungle");
+  });
+  it("should check the state of searchText", () => {
+    wrapper
+      .find("input.searchtext")
+      .simulate("change", { target: { name: "search", value: "jungle" } });
+    expect(wrapper.state("searchText")).toEqual("jungle");
+  });
+  it("should call mock function when button is clicked", () => {
+    const mockFn = jest.fn();
+    const tree = shallow(<button name="button test" handleHandler={mockFn} />);
+    tree.simulate("click");
+    mockFn();
+    expect(mockFn).toHaveBeenCalled();
+  });
+  it("should check the state of preSearchText", () => {
+    wrapper
+      .find("input.searchtext")
+      .simulate("change", { target: { name: "search", value: "jungle" } });
+    wrapper.find("button").simulate("click");
+    expect(wrapper.state("prevSearchText")).toEqual("jungle");
+  });
+  it("should check searching for the same text as previous", () => {
+    wrapper
+      .find("input.searchtext")
+      .simulate("change", { target: { name: "search", value: "jungle" } });
+    wrapper.find("button").simulate("click");
+    wrapper.find("button").simulate("click");
+    expect(wrapper.state("prevSearchText")).toEqual(
+      wrapper.state("searchText")
+    );
   });
 });
